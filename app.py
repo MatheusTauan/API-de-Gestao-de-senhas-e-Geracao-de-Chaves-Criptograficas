9e90124496fa305938c7d6b6b65dcba4531a25d5
MatheusTauan <matheus-tauan@hotmail.com>
2024-04-09 14:40:50 -0300

    verificador de senhas e criador de senhas implementados

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import string
+import random
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return 'A API está online'
+
+@app.route("/criarsenha")
+def gerador_de_senhas():
+    comprimento = int(request.args.get('comprimento', 16))
+    senha = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(comprimento))
+    return senha
+
+@app.route("/verificarsenha")
+def verificador_de_senha():
+    senha = request.args.get('senha')
+    
+    if len(senha) < 16:
+        return jsonify({"valido": False, "mensagem": "A senha deve ter pelo menos 16 caracteres."})
+    if not re.search(r'[A-Z]', senha):
+        return jsonify({"valido": False, "mensagem": "A senha deve conter pelo menos uma Letra maiuscula."})
+    if not re.search(r'[a-z]', senha):
+        return jsonify({"valido": False, "mensagem": "A senha deve conter pelo menos uma Letra minuscula."})
+    if not re.search(r'\d', senha):
+        return jsonify({"valido": False, "mensagem": "A senha deve conter pelo menos um digito."})
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', senha):
+        return jsonify({"valido": False, "mensagem": 'A senha deve conter pelo menos um caracter especial.'})
+    
+    return jsonify({"valido": True, "mensagem": "A senha atende a todos os requisitos."})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
